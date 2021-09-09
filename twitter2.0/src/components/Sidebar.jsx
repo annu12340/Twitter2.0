294880d1fe7c8f49a4a4a64bc028d571077c6ceb
annu12340 <annujolly17@gmail.com>
2021-09-09 12:53:10 +0530

    completed the sidebar

--- twitter2.0/src/components/Sidebar.jsx
+++ twitter2.0/src/components/Sidebar.jsx
@@ -1,19 +1,38 @@
 import React from "react";
+import SidebarIcon from "./SidebarIcon";
 import "../style/Sidebar.css";
 
 import TwitterIcon from "@material-ui/icons/Twitter";
-import SidebarIcon from "./SidebarIcon";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import NotificationsNoneIcon from "@material-ui/icons/NotificationsNone";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import PermIdentityIcon from "@material-ui/icons/PermIdentity";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import { Button } from "@material-ui/core";
 
 const Sidebar = () => {
    return (
       <div>
          {/* Logo */}
          <TwitterIcon />
+
          {/* Navbar items */}
-         <SidebarIcon />
-         <SidebarIcon />
-         <SidebarIcon />
+         <SidebarIcon active Icon={HomeIcon} text='Home' />
+         <SidebarIcon Icon={SearchIcon} text='Explore' />
+         <SidebarIcon Icon={NotificationsNoneIcon} text='Notifications' />
+         <SidebarIcon Icon={MailOutlineIcon} text='Messages' />
+         <SidebarIcon Icon={BookmarkBorderIcon} text='Bookmarks' />
+         <SidebarIcon Icon={ListAltIcon} text='Lists' />
+         <SidebarIcon Icon={PermIdentityIcon} text='Profile' />
+         <SidebarIcon Icon={MoreHorizIcon} text='More' />
+
          {/* Tweet option */}
+         <Button variant='outlined' className='sidebar__tweet' fullWidth>
+            Tweet
+         </Button>
       </div>
    );
 };
--- twitter2.0/src/components/SidebarIcon.jsx
+++ twitter2.0/src/components/SidebarIcon.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "../style/SidebarIcon.css";
 
-function SidebarIcon() {
-   return <div>hello</div>;
+function SidebarIcon({ active, text, Icon }) {
+   return (
+      <div className={`sidebarOption ${active && "sidebarOption--active"}`}>
+         <Icon />
+         <h2>{text}</h2>
+      </div>
+   );
 }
 
 export default SidebarIcon;

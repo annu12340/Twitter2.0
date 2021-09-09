a1ee31211a35266c3acf7c0289a5b318f3deb3ef
annu12340 <annujolly17@gmail.com>
2021-09-09 13:54:58 +0530

    Working on individual post on a feed

--- src/components/Feed/Posts.jsx
+++ src/components/Feed/Posts.jsx
@@ -1,10 +1,38 @@
 import React from "react";
 import "../../style/Posts.css";
-function Posts() {
+function Posts({ displayName, username, verified, text, image, avatar }) {
    return (
-      <div>
-         Home s{/* Area to make tweet */}
-         {/* All tweets */}
+      <div className='post'>
+         {/* Header */}
+         <div className='post__avatar'>
+            <Avatar src={avatar} />
+         </div>
+
+         {/* Body */}
+         <div className='post__body'>
+            <div className='post__header'>
+               <div className='post__headerText'>
+                  <h3>
+                     {displayName}{" "}
+                     <span className='post__headerSpecial'>
+                        {verified && <VerifiedUserIcon className='post__badge' />} @{username}
+                     </span>
+                  </h3>
+               </div>
+               <div className='post__headerDescription'>
+                  <p>{text}</p>
+               </div>
+            </div>
+            <img src={image} alt='' />
+
+            {/* Footer */}
+            <div className='post__footer'>
+               <ChatBubbleOutlineIcon fontSize='small' />
+               <RepeatIcon fontSize='small' />
+               <FavoriteBorderIcon fontSize='small' />
+               <PublishIcon fontSize='small' />
+            </div>
+         </div>
       </div>
    );
 }
